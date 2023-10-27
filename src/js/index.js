f3adc8923f09a2e3a0d101f7db21c0c3066a0e2c
Sergey UP <sergey10goals@gmail.com>
2023-10-27 12:03:03 +0300

    add mobile nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,12 @@
-// import mobileNav from './modules/mobile-nav.js';
 import { animation } from "./modules/animation";
 import { autoCompleteFunc } from "./modules/autoComplete";
 import { datePicker } from "./modules/datePicker";
+import { mobileNav } from "./modules/mobile-nav";
 import { slider } from "./modules/slider";
 import { sliderTestimonials } from "./modules/sliderTestimonials";
 
 const init = () => {
-  // mobileNav();
-
+  mobileNav();
   autoCompleteFunc();
   datePicker();
   slider();
